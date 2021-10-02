d069c201c8fc097ce609a37a4ddd9483290c5e95
Paulo Henrique <paulohenriqe226@gmail.com>
2021-10-02 19:20:36 -0300

    Author and Book created

---
+++ src/main/java/com/cursospring/curso/entity/Author.java
@@ -0,0 +1,28 @@
+package com.cursospring.curso.entity;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data  // lombok getters and setters
+@Builder // automatically produce the code required to have your class be instantiable with code
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity  // author class is a entity
+public class Author {
+
+    @Id  // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // for every author add, auto increments
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
---
+++ src/main/java/com/cursospring/curso/entity/Book.java
@@ -0,0 +1,41 @@
+package com.cursospring.curso.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data  // lombok getters and setters
+@Builder // automatically produce the code required to have your class be instantiable with code
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "publisher_name", nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})  // MANY books can be written by ONE author
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+}
